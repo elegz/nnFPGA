18777fa8845fb50763deb336316df94a7d58be74
elegz <al.kotelnikov@gmail.com>
2016-10-18 19:53:30 +0300

    Updates conv3d to max_pool interface and new win_pad (slow stride).

--- convolution/rtl/conv3d.sv
+++ convolution/rtl/conv3d.sv
@@ -4,6 +4,7 @@
 module conv3d import functions_pkg::clog2; # (
    parameter FRAME_H_MAX   = 224,
    parameter FRAME_W_MAX   = 224,
+   parameter STRIDE_MAX    = 4,
    parameter DIN_WIDTH     = 8,
    parameter DOUT_WIDTH    = 8,
    parameter KERN_WIDTH    = 16,
@@ -17,24 +18,29 @@ module conv3d import functions_pkg::clog2; # (
    //external buffer
    input    wire [clog2(FRAME_H_MAX):0] frame_h,
    input    wire [clog2(FRAME_W_MAX):0] frame_w,
+   input    wire [ clog2(STRIDE_MAX):0] stride,
 
    input    wire                                      clk,
    input    wire                                      reset_n,
    input    wire                                      frame_start,
    input    wire                                      din_vld,
    input    wire [ CHANNELS_IN-1:0][ DIN_WIDTH-1:0]   din,
-   output   wire                                      dout_vld[CHANNELS_OUT],
-   output   wire                   [DOUT_WIDTH-1:0]   dout[CHANNELS_OUT]
+   output   wire                                      dout_vld,
+   output   wire [CHANNELS_OUT-1:0][DOUT_WIDTH-1:0]   dout
 );
    wire                                                                   frame_start_buf;
    wire                                                                   din_vld_buf;
    wire                   [CHANNELS_IN-1:0][WIN_SIZE-1:0][DIN_WIDTH-1:0]  din_buf;
    wire                                                                   win_vld;
    wire  [CHANNELS_IN-1:0][   WIN_SIZE-1:0][WIN_SIZE-1:0][DIN_WIDTH-1:0]  window;
+   wire                                                                   conv_dout_vld[CHANNELS_OUT];
+
+   assign dout_vld = conv_dout_vld[0];
 
    row_buffer # (
       .FRAME_H_MAX      (FRAME_H_MAX),
       .FRAME_W_MAX      (FRAME_W_MAX),
+      .STRIDE_MAX       (STRIDE_MAX),
       .DIN_WIDTH        (DIN_WIDTH),
       .WIN_SIZE         (WIN_SIZE),
       .CH_NUM           (CHANNELS_IN)
@@ -55,8 +61,6 @@ module conv3d import functions_pkg::clog2; # (
       .FRAME_H_MAX   (FRAME_H_MAX),
       .FRAME_W_MAX   (FRAME_W_MAX),
       .DIN_WIDTH     (DIN_WIDTH),
-      .FRAME_H       (FRAME_H),
-      .FRAME_W       (FRAME_W),
       .WIN_SIZE      (WIN_SIZE),
       .CH_NUM        (CHANNELS_IN)
    ) win_pad_inst (
@@ -64,6 +68,7 @@ module conv3d import functions_pkg::clog2; # (
       .reset_n,
       .frame_h,
       .frame_w,
+      .stride,
       .frame_start   (frame_start_buf),
       .din_vld       (din_vld_buf),
       .din           (din_buf),
@@ -87,7 +92,7 @@ module conv3d import functions_pkg::clog2; # (
             .kernel     (kernel[g]),
             .din_vld    (win_vld),
             .din        (window),
-            .dout_vld   (dout_vld[g]),
+            .dout_vld   (conv_dout_vld[g]),
             .dout       (dout[g])
          );
       end: ch_out
